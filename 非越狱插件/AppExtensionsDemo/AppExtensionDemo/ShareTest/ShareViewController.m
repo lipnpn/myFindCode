7752b5e742dd566dd92e4bc3eaf1a1aba809e76f
lipengpeng <lipengpeng@mobimagic.com>
2016-01-20 18:06:29 +0800

    cocoahttp server
    cocoahttp server

--- 非越狱插件/AppExtensionsDemo/AppExtensionDemo/ShareTest/ShareViewController.m
+++ 非越狱插件/AppExtensionsDemo/AppExtensionDemo/ShareTest/ShareViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ShareViewController.h"
+static NSInteger const maxCharactersAllowed =  140;//手动设置字符数上限
 
 @interface ShareViewController ()
 
@@ -16,15 +17,21 @@
 
 - (BOOL)isContentValid {
     // Do validation of contentText and/or NSExtensionContext attachments here
+    NSInteger length = self.contentText.length;
+    self.charactersRemaining = @(maxCharactersAllowed - length);
+    if (self.charactersRemaining.intValue < 0) {
+        return NO;
+    }
     return YES;
 }
-
 - (void)didSelectPost {
     // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
     
     // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
     [self.extensionContext completeRequestReturningItems:@[] completionHandler:nil];
 }
+
+
 
 - (NSArray *)configurationItems {
     // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
